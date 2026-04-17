b6322b8bd784a6ab81ef5622fb075d73e2e8984f
Xeno <226741508+XenoLeads@users.noreply.github.com>
2026-04-17 15:37:11 +0530

    feat(ui): add functionality to submit a work order

--- src/app/report/page.tsx
+++ src/app/report/page.tsx
@@ -1,15 +1,14 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import type { Asset } from "@/types";
+import type { Asset as AssetType, WorkOrderPriority as WorkOrderPriorityType } from "@/types";
+const BACKEND_URL = process.env.NEXT_PUBLIC_API_ENDPOINT_URL;
 
 const Page = () => {
-  const [query, setQuery] = useState("");
-  const [assets, setAssets] = useState<Asset[]>([]);
-
-  // Todo:
-  // 1. Capture the inputs
-  // 2. Send a post request to add a new work order
+  const [assetTag, setAssetTag] = useState("");
+  const [assets, setAssets] = useState<AssetType[]>([]);
+  const [workOrderPriority, setWorkOrderPriority] = useState<WorkOrderPriorityType>("LOW");
+  const [workOrderIssueDesc, setWorkOrderIssueDesc] = useState("");
 
   useEffect(() => {
     async function fetchAndSetAssets() {
@@ -22,6 +21,25 @@ const Page = () => {
     fetchAndSetAssets();
   }, []);
 
+  async function handleWorkOrderSubmission() {
+    const assetId = assets.find(asset => asset.assetTag === assetTag)?.id;
+    if (!assetId) return;
+    const response = await fetch(`${BACKEND_URL}/work-orders`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ assetId, priority: workOrderPriority, issueDesc: workOrderIssueDesc }),
+    });
+    const json = await response.json();
+    if (json.success) clearInputs();
+    console.log(json);
+  }
+
+  function clearInputs() {
+    setWorkOrderPriority("LOW");
+    setWorkOrderIssueDesc("");
+    setAssetTag("");
+  }
+
   return (
     <div className="text-white h-full w-full flex justify-center items-center">
       <div className="bg-neutral-800 h-8/10 w-8/10 rounded-sm border flex flex-col justify-stretch p-4 text-xl gap-4">
@@ -31,8 +49,8 @@ const Page = () => {
           className="border-2 rounded-sm p-2"
           placeholder="Asset Name..."
           type="text"
-          value={query}
-          onChange={e => setQuery(e.target.value)}
+          value={assetTag}
+          onChange={e => setAssetTag(e.target.value)}
           name="query"
           id="query"
           list="query-options"
@@ -44,14 +62,29 @@ const Page = () => {
             </option>
           ))}
         </datalist>
-        <select className="bg-neutral-800 border-2 rounded-sm p-2" name="priority" id="priority">
+        <select
+          className="bg-neutral-800 border-2 rounded-sm p-2"
+          name="priority"
+          id="priority"
+          value={workOrderPriority}
+          onChange={e => setWorkOrderPriority(e.target.value as WorkOrderPriorityType)}
+        >
           <option value="LOW">Low</option>
           <option value="MEDIUM">Medium</option>
           <option value="HIGH">High</option>
           <option value="DOWNTIME">Downtime</option>
         </select>
-        <textarea className="w-full border-2 rounded-sm p-2" placeholder="Issue Description..." name="issueDesc" id="issueDesc"></textarea>
-        <button className="bg-neutral-700 py-4 rounded-sm cursor-pointer border">Submit</button>
+        <textarea
+          className="w-full border-2 rounded-sm p-2"
+          placeholder="Issue Description..."
+          name="issueDesc"
+          id="issueDesc"
+          value={workOrderIssueDesc}
+          onChange={e => setWorkOrderIssueDesc(e.target.value)}
+        />
+        <button className="bg-neutral-700 py-4 rounded-sm cursor-pointer border" onClick={handleWorkOrderSubmission}>
+          Submit
+        </button>
       </div>
     </div>
   );
