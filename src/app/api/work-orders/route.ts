6a914c82eec0dd1ddb1e22c76f54e2f1635cff8b
Xeno <226741508+XenoLeads@users.noreply.github.com>
2026-04-17 11:56:50 +0530

    feat(api): add endpoint to submit a new work order

--- src/app/api/work-orders/route.ts
+++ src/app/api/work-orders/route.ts
@@ -1,7 +1,16 @@
 import { NextResponse } from "next/server"
-import { getAllWorkOrders } from "@/lib/work-orders"
+import { getAllWorkOrders, submitWorkOrder } from "@/lib/work-orders"
 
 export async function GET() {
   const workOrders = await getAllWorkOrders()
+  return NextResponse.json({ success: true, data: workOrders })
+}
+
+export async function POST(request: Request) {
+  const body = await request.json()
+  const requiredProperties = ["issueDesc", "priority", "assetId"]
+  if (!requiredProperties.every(property => property in body)) return NextResponse.json({ success: false, message: "Some fields are missing" })
+  const { issueDesc, priority, assetId } = body
+  const workOrders = await submitWorkOrder({ issueDesc, priority, assetId })
   return NextResponse.json({ success: true, data: workOrders })
 }
--- src/lib/work-orders.ts
+++ src/lib/work-orders.ts
@@ -1,6 +1,17 @@
 import { prisma } from "@/lib/prisma"
+import type { WorkOrder } from "@/types"
 
 export async function getAllWorkOrders() {
   const workOrders = await prisma.workOrder.findMany()
+  return workOrders
+}
+export async function submitWorkOrder({ issueDesc, priority, assetId }: WorkOrder) {
+  const workOrders = await prisma.workOrder.create({
+    data: {
+      issueDesc,
+      priority,
+      assetId
+    }
+  })
   return workOrders
 }
